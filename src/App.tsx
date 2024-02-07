30a2de89add3780daa21a211ab07787d86e7154f
Mark Haddleton <markwhaddleton@gmail.com>
2024-02-06 22:38:32 -0500

    Added Portrait and New Cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import { Box, Card, Flex, Heading, Text } from '@chakra-ui/react'
+import { Box, Card, Flex, Heading, Image, SimpleGrid, Text } from '@chakra-ui/react'
 import { useState } from 'react'
 import ProjectCard from './ProjectCard';
+import Portrait from './assets/portrait.svg'
 
 type Project = {
   projectName: string;
   description: string;
-  githubLink: string;
+  githubLink?: string
 }
 
 const projectInfo: Project[] = [
   {
     projectName: "Forge Website",
-    description: "A Work-In-Progress overhaul for the website of The Forge, managing makerspace machine usage and membership for 300+ students",
+    description: "A Work-In-Progress overhaul for the website of The Forge, managing makerspace machine usage and membership for 300+ students.",
     githubLink: "https://github.com/BreadInvasion/forge-new-website"
   },
   {
     projectName: "HelpAlign",
     description: "My RPI Hackathon X project - my goal was to build a service where people could find therapists in their area and securely message them.",
     githubLink: "https://github.com/BreadInvasion/HelpAlign"
+  },
+  {
+    projectName: "This Website!",
+    description: "Built in roughly an hour, just to have some brief summaries and links to my work all in one place!",
+    githubLink: "https://github.com/BreadInvasion/portfolio"
+  },
+  {
+    projectName: "DayDreamers",
+    description: "A semester-long project for Software Design and Documentation, in which I built a calendar app with online storage and user invites with the help of a team.",
+    githubLink: "https://github.com/BreadInvasion/Day-Dreamers"
+  },
+  {
+    projectName: "Boeing SIRA",
+    description: "My master's research project in collaboration with Boeing. The Semi-Independent Robotic Assistance project used computer vision to identify and manipulate 100+-pound cables!"
   }
 ]
 
@@ -25,9 +40,10 @@ function App() {
   return (
     <Box textAlign={"center"}>
       <Heading marginTop={12} size={"4xl"}>Mark Haddleton</Heading>
-      <Flex>
+      <Image maxW={"25%"} marginLeft={"auto"} marginRight={"auto"} borderRadius={20} src={Portrait}/>
+      <SimpleGrid spacing={4} columns={3} maxWidth={"70%"} marginLeft={"auto"} marginRight={"auto"} marginTop={8}>
         {projectInfo.map((value: Project) => {return <ProjectCard projectName={value.projectName} description={value.description} githubLink={value.githubLink} />})}
-      </Flex>
+      </SimpleGrid>
     </Box>
   )
 }
--- src/ProjectCard.tsx
+++ src/ProjectCard.tsx
@@ -1,11 +1,11 @@
 import { Card, Heading, IconButton, Text } from "@chakra-ui/react";
 import { FaGithub } from "react-icons/fa";
 
-function ProjectCard(props: {projectName: string, description: string, githubLink: string}) {
-    return <Card width={80} height={64} padding={8} margin={4}>
+function ProjectCard(props: {projectName: string, description: string, githubLink?: string}) {
+    return <Card width={80} height={72} padding={8} margin={4}>
         <Heading>{props.projectName}</Heading>
         <Text marginTop={4}>{props.description}</Text>
-        <IconButton position={"absolute"} right={4} bottom={4} maxW={12} as={"a"} href={props.githubLink} icon={<FaGithub />} aria-label={"Github link to " + props.projectName} />
+        {props.githubLink && <IconButton position={"absolute"} right={4} bottom={4} maxW={12} as={"a"} href={props.githubLink} target="_blank" rel="noreferrer noopener" icon={<FaGithub />} aria-label={"Github link to " + props.projectName} />}
     </Card>
 }
 
